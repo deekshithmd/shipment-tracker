51df3c57954ecf78a8564b0e042212d4a25ae2a9
deekshithepns <70252174+deekshithmd@users.noreply.github.com>
2023-07-10 23:02:09 +0530

    added index file

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 
 import { TrackingContextProvider } from "@/context/Tracking";
+import { NavBar, Footer } from "@/components";
 
 export default function App({ Component, pageProps }) {
   return (
     <TrackingContextProvider>
+      <NavBar />
       <Component {...pageProps} />
+      <Footer />
     </TrackingContextProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const index=()=>{
-  return <div>Hello</div>
-}
+import { useTrackingContext } from "@/context/Tracking";
+
+import {
+  Table,
+  Form,
+  Services,
+  Profile,
+  CompleteShipment,
+  GetShipment,
+  StartShipment,
+} from "@/components";
+
+const index = () => {
+  const {
+    currentUser,
+    createShipment,
+    getAllShipment,
+    getShipment,
+    completeShipment,
+    startShipment,
+    getShipmentCount,
+  } = useTrackingContext();
+  const [createShipmentModal, setCreateShipmentModal] = useState(false);
+  const [openProfile, setOpenProfile] = useState(false);
+  const [startModal, setStartModal] = useState(false);
+  const [completeModal, setCompleteModal] = useState(false);
+  const [getModal, setGetModal] = useState(false);
+
+  const [allShipmentData, setAllShipmentData] = useState();
+
+  useEffect(() => {
+    const getCampaignsData = getAllShipment();
+
+    return async () => {
+      const allData = await getCampaignsData;
+      setAllShipmentData(allData);
+    };
+  });
+
+  return <div>Hello</div>;
+};
 
 export default index;
