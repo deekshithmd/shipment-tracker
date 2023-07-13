7856eb0f8e36cbfffb0939a20449bf8407349a3b
deekshithepns <70252174+deekshithmd@users.noreply.github.com>
2023-07-13 23:22:46 +0530

    added table

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,7 +1,68 @@
 import React from "react";
+import images from "../Images";
+import Image from "next/image";
 
-const Services = () => {
-  return <div>Services</div>;
+const Services = ({
+  setOpenProfile,
+  setCompleteModal,
+  setGetModal,
+  setStartModal,
+}) => {
+  const team = [
+    {
+      avatar: images.compShipment,
+    },
+    {
+      avatar: images.getShipment,
+    },
+    {
+      avatar: images.startShipment,
+    },
+    {
+      avatar: images.shipCount,
+    },
+    {
+      avatar: images.send,
+    },
+  ];
+
+  const openModalBox = (text) => {
+    if (text === 1) {
+      setCompleteModal(true);
+    } else if (text === 2) {
+      setGetModal(true);
+    } else if (text === 3) {
+      setStartModal(true);
+    } else if (text === 4) {
+      setOpenProfile(true);
+    }
+  };
+  return (
+    <section className="py-0 pb-14">
+      <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+        <div className="mt-12 ">
+          <ul className="grid gap-8 sm:grid-cols-2 md:grid-cols-3">
+            {team.map((item, idx) => {
+              return (
+                <li key={idx}>
+                  <div
+                    className="w-full h-60 sm:h-52 md:h-56"
+                    onClick={() => openModalBox(i + 1)}
+                  >
+                    <Image
+                      src={item.avatar}
+                      className="w-full h-full object-cover object-center shadow-md rounded-xl"
+                      alt=""
+                    />
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
